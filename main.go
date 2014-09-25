6e6a6f1f00a7efdfd15f24ea248db60bae697b14
Alexander Rødseth <alexander.rodseth@usit.uio.no>
2014-09-25 16:26:52 +0200

    Beginnings of a dashboard

--- main.go
+++ main.go
@@ -3,128 +3,133 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/codegangsta/negroni"
+	"github.com/go-martini/martini"
 	"github.com/unrolled/render"
-	"github.com/xyproto/permissions"
-	"github.com/xyproto/webhandle"
+	"github.com/xyproto/fizz"
 )
 
 const (
 	Version = "0.1"
 )
 
+type MenuItem struct {
+	Url      string
+	Text     string
+	Selected bool
+}
+
+type Menu struct {
+	Title     string
+	MenuItems []MenuItem
+}
+
+type Post struct {
+	Title string
+	Body  string
+}
+
+type PageData struct {
+	Text  map[string]string
+	Menu  Menu
+	Posts []Post
+}
+
+func GenerateMenu(active int) Menu {
+
+	// Create a menu
+	var menu Menu
+	menu.Title = "Menu"
+
+	var menuitem1 MenuItem
+	menuitem1.Url = "/"
+	menuitem1.Text = "Dashboard"
+	if active == 0 {
+		menuitem1.Selected = true
+	}
+
+	var menuitem2 MenuItem
+	menuitem2.Url = "/mirrors"
+	menuitem2.Text = "Mirrors"
+	if active == 1 {
+		menuitem2.Selected = true
+	}
+
+	var menuitem3 MenuItem
+	menuitem3.Url = "/forum"
+	menuitem3.Text = "Forum"
+	if active == 2 {
+		menuitem3.Selected = true
+	}
+
+	var menuitem4 MenuItem
+	menuitem4.Url = "/wiki"
+	menuitem4.Text = "Wiki"
+	if active == 3 {
+		menuitem4.Selected = true
+	}
+
+	menu.MenuItems = append(menu.MenuItems, menuitem1)
+	menu.MenuItems = append(menu.MenuItems, menuitem2)
+	menu.MenuItems = append(menu.MenuItems, menuitem3)
+	menu.MenuItems = append(menu.MenuItems, menuitem4)
+
+	return menu
+}
+
 func main() {
 	fmt.Println("dashboard ", Version)
 
 	r := render.New(render.Options{})
 
-	n := negroni.Classic()
+	m := martini.Classic()
 
-	perm := permissions.New()
-	userstate := perm.UserState()
+	fizz := fizz.New()
 
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		data := map[string]string{
-			"version_string": "0.1",
-			"server":         "server",
-			"status":         "up",
-			"shortname":      "dx",
-			"statuscolor":    "#a0a0ff",
+	// Dashboard
+	m.Get("/", func(w http.ResponseWriter, req *http.Request) {
+		var page PageData
+		page.Text = map[string]string{
+			"description": "Dashboard",
+			"title":       "Dashboard",
+			"subtitle":    "Charts and graphs",
 		}
+
+		// Generate a menu where item 0 is active
+		page.Menu = GenerateMenu(0)
 
 		// !! Reload template !!
 		r = render.New(render.Options{})
 
-		// Render and return
-		r.HTML(w, http.StatusOK, "index", data)
+		// Render the specified templates/.tmpl file as HTML and return
+		r.HTML(w, http.StatusOK, "dashboard", page)
 	})
 
-	mux.HandleFunc("/status", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Has user bob: %v\nLogged in on server: %v\n", userstate.HasUser("bob"), userstate.IsLoggedIn("bob"))
-		fmt.Fprintf(w, "Is confirmed: %v\n", userstate.IsConfirmed("bob"))
-		fmt.Fprintf(w, "Current user is logged in, has a valid cookie and *user rights*: %v\n", userstate.UserRights(req))
-		fmt.Fprintf(w, "Current user is logged in, has a valid cookie and *admin rights*: %v\n", userstate.AdminRights(req))
-		fmt.Fprintf(w, "Username stored in cookies (or blank): %v\n", userstate.GetUsername(req))
-		fmt.Fprint(w, "\nTry: /register, /confirm, /remove, /login, /logout, /makeadmin and /admin")
-	})
-
-	mux.HandleFunc("/hello/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello, %v", webhandle.GetLast(req.URL))
-	})
-
-	mux.HandleFunc("/register", func(w http.ResponseWriter, req *http.Request) {
-		userstate.AddUser("bob", "hunter1", "bob@zombo.com")
-		fmt.Fprintf(w, "User bob was created: %v\n", userstate.HasUser("bob"))
-	})
-
-	mux.HandleFunc("/confirm", func(w http.ResponseWriter, req *http.Request) {
-		userstate.MarkConfirmed("bob")
-		fmt.Fprintf(w, "User bob was confirmed: %v\n", userstate.IsConfirmed("bob"))
-	})
-
-	mux.HandleFunc("/remove", func(w http.ResponseWriter, req *http.Request) {
-		userstate.RemoveUser("bob")
-		fmt.Fprintf(w, "User bob was removed: %v\n", !userstate.HasUser("bob"))
-	})
-
-	mux.HandleFunc("/login", func(w http.ResponseWriter, req *http.Request) {
-		userstate.Login(w, "bob")
-		fmt.Fprintf(w, "bob is now logged in: %v\n", userstate.IsLoggedIn("bob"))
-	})
-
-	mux.HandleFunc("/logout", func(w http.ResponseWriter, req *http.Request) {
-		userstate.Logout("bob")
-		fmt.Fprintf(w, "bob is now logged out: %v\n", !userstate.IsLoggedIn("bob"))
-	})
-
-	mux.HandleFunc("/makeadmin", func(w http.ResponseWriter, req *http.Request) {
-		userstate.SetAdminStatus("bob")
-		fmt.Fprintf(w, "bob is now administrator: %v\n", userstate.IsAdmin("bob"))
-	})
-
-	mux.HandleFunc("/data", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "user page that only logged in users must see!")
-	})
-
-	mux.HandleFunc("/admin", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "super secret information that only logged in administrators must see!\n\n")
-		usernames, err := userstate.GetAllUsernames()
-		if err == nil {
-			fmt.Fprintf(w, "list of all users: "+strings.Join(usernames, ", "))
+	m.Get("/mirrors", func(w http.ResponseWriter, req *http.Request) {
+		var page PageData
+		page.Text = map[string]string{
+			"description": "Mirrors",
+			"title":       "Mirrors",
+			"subtitle":    "Mirror mirror on the wall",
 		}
+
+		// Generate a menu where item 1 is active
+		page.Menu = GenerateMenu(1)
+
+		// !! Reload template !!
+		r = render.New(render.Options{})
+
+		// Render the specified templates/.tmpl file as HTML and return
+		r.HTML(w, http.StatusOK, "mirrors", page)
 	})
 
-	// Publish the generated SVG as "/circles.svg"
-	svgurl := "/circles.svg"
-	mux.HandleFunc(svgurl, func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content-Type", "image/svg+xml")
-		fmt.Fprint(w, svgPage().String())
-	})
-
-	// Generate a Page that includes the svg image
-	page := indexPage(svgurl)
-	// Publish the generated Page in a way that connects the HTML and CSS
-	page.Publish(mux, "/html", "/style.css", false)
-
-	// Permission middleware
-
-	perm.SetDenyFunction(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Permission denied!")
-	})
+	// TODO: Admin panel with simple user management
 
 	// Activate the permission middleware
+	m.Use(fizz.All())
 
-	n.Use(perm)
+	// Share the files in static
+	m.Use(martini.Static("static"))
 
-	// Already included in negroni.Classic:
-	//// Share the files in static
-	//n.Use(negroni.NewStatic(http.Dir("static")))
-
-	n.UseHandler(mux)
-
-	n.Run(":3000")
+	m.Run() // port 3000 by default
 }
