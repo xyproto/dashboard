63052d343d04647a4f5ed120370c32619b528a41
Alexander F Rødseth <rodseth@gmail.com>
2015-04-22 09:57:49 +0200

    Minor change

--- compileloop.sh
+++ compileloop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SOURCE=
-for f in *.go ../onthefly/*.go ../genericsite/*.go ../instapage/*.go ../simpleredis/*.go ../siteengines/*.go ../moskus/*.go ../webhandle/*.go; do
+for f in *.go ../onthefly/*.go ../genericsite/*.go ../simpleredis/*.go ../siteengines/*.go ../moskus/*.go ../webhandle/*.go; do
   SOURCE+="$f "
 done
 BIN=dashboard
